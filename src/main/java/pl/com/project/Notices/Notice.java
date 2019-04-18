7672af95ea37ff5e22586a3d996d0ec35ae78f3b
Piotr Kwasek <kwasekpiotr98@gmail.com>
2019-04-18 12:20:12 +0200

    test

--- src/main/java/pl/com/project/Notices/Notice.java
+++ src/main/java/pl/com/project/Notices/Notice.java
@@ -11,23 +11,13 @@ public class Notice {
     @GeneratedValue
     private Long id;
 
-
-    private char look_or_offer;
-    private Date meeting_date;
     private String meeting_place;
-    private StringBuffer note = new StringBuffer(500);
-    private Long user_id_user;
 
-    public Notice(char look_or_offer, Date meeting_date, String meeting_place, StringBuffer note, Long user_id_user) {
-        this.look_or_offer = look_or_offer;
-        this.meeting_date = meeting_date;
+    public Notice(String meeting_place) {
         this.meeting_place = meeting_place;
-        this.note = note;
-        this.user_id_user = user_id_user;
     }
+    public Notice() {};
 
-    public Notice() {
-    }
 
     public Long getId() {
         return id;
@@ -37,22 +27,6 @@ public class Notice {
         this.id = id;
     }
 
-    public char getLook_or_offer() {
-        return look_or_offer;
-    }
-
-    public void setLook_or_offer(char look_or_offer) {
-        this.look_or_offer = look_or_offer;
-    }
-
-    public Date getMeeting_date() {
-        return meeting_date;
-    }
-
-    public void setMeeting_date(Date meeting_date) {
-        this.meeting_date = meeting_date;
-    }
-
     public String getMeeting_place() {
         return meeting_place;
     }
@@ -60,20 +34,4 @@ public class Notice {
     public void setMeeting_place(String meeting_place) {
         this.meeting_place = meeting_place;
     }
-
-    public Long getUser_id_user() {
-        return user_id_user;
-    }
-
-    public void setUser_id_user(Long user_id_user) {
-        this.user_id_user = user_id_user;
-    }
-    public StringBuffer getNote() {
-        return note;
-    }
-
-    public void setNote(StringBuffer note) {
-        this.note = note;
-    }
-
 }
