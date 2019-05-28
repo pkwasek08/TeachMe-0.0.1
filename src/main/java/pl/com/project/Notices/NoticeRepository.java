5b79fe5e3728cb07839b61548bbaeae437fe6db0
Piotr Kwasek <kwasekpiotr98@gmail.com>
2019-05-29 00:08:46 +0200

    NoticeEntity

--- src/main/java/pl/com/project/Notices/NoticeRepository.java
+++
@@ -1,6 +0,0 @@
-package pl.com.project.Notices;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface NoticeRepository extends CrudRepository<Notice, Long> {
-}
--- src/main/java/pl/com/project/Users/PeopleRepository.java
+++
@@ -1,6 +0,0 @@
-package pl.com.project.Users;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PeopleRepository extends CrudRepository<Person, Long> {
-}
