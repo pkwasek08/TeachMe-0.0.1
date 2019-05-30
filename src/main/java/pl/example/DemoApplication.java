ad88072119497fcb95a60b0a68a691a3a2c6b990
Piotr Kwasek <kwasekpiotr98@gmail.com>
2019-05-30 13:19:27 +0200

    test

--- src/main/java/pl/example/DemoApplication.java
+++ src/main/java/pl/example/DemoApplication.java
@@ -2,16 +2,6 @@ package pl.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
 
 @SpringBootApplication
 public class DemoApplication {
--- src/main/java/pl/example/controllers/HelloController.java
+++ src/main/java/pl/example/controllers/HelloController.java
@@ -3,6 +3,7 @@ package pl.example.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.example.repository.UserRepository;
@@ -25,4 +26,9 @@ public class HelloController {
         return "Hi";
     }
 
+    @GetMapping("hi")
+    public String hello(Model model1) {
+        model1.addAttribute("user", userRepository.findAll());
+        return "hello";
+    }
 }
