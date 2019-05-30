e84a45a404e5a15234103d2012918a047e61dc58
Piotr Kwasek <kwasekpiotr98@gmail.com>
2019-05-30 12:29:13 +0200

    cors

--- src/main/java/pl/example/DemoApplication.java
+++ src/main/java/pl/example/DemoApplication.java
@@ -20,30 +20,5 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@EnableWebSecurity
-	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
-			http.csrf().disable();
-		}
-	}
-
-	@Configuration
-	public class MyConfiguration {
-
-		@Bean
-		public WebMvcConfigurer corsConfigurer() {
-			return new WebMvcConfigurerAdapter() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-				}
-
-			};
-		}
-	}
 
 }
