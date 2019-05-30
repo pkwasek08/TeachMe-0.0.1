0e7e8ca71302a821312ae43923888d5bbe7732de
Piotr Kwasek <kwasekpiotr98@gmail.com>
2019-05-30 12:21:37 +0200

    cors

--- src/main/java/pl/example/controllers/HelloController.java
+++ src/main/java/pl/example/controllers/HelloController.java
@@ -1,12 +1,14 @@
 package pl.example.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/")
+    @CrossOrigin(origins = "*")
     public String sayHI()
     {
         return "Hi";
